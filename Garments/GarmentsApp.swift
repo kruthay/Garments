4497e2e0037037ded9c4442d1f1e0dc3db7874a0
Kruthay Donapati <117230425+kruthay@users.noreply.github.com>
2023-08-30 10:21:52 -0400

    Initial Commit

---
+++ Garments/GarmentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GarmentsApp.swift
+//  Garments
+//
+//  Created by Kruthay Donapati on 8/30/23.
+//
+
+import SwiftUI
+
+@main
+struct GarmentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
